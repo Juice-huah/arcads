a7e523244c3d96c2da7b7325805e120ea352bfa6
Jushua <jushuaostraes@gmail.com>
2026-04-14 10:05:57 +0800

    Fixed hamsterball bug

--- src/pages/CreateHamsterBall.jsx
+++ src/pages/CreateHamsterBall.jsx
@@ -115,7 +115,7 @@ function CreateHamsterBall() {
             body: JSON.stringify({
               teacher_fid: user.uid,
               class_id: classId,
-              game_type: 'Hamster Ball',
+              game_type: 'hamster_ball',
               custom_title: gameTitle, // 🟢 FIXED: Sends as custom_title to match the DB
               open_datetime: formattedOpenDate,
               close_datetime: formattedCloseDate,
